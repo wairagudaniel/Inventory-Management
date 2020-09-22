939d588d0d12e309859a947e45ff85c0e904e251
wairagudaniel <danielwairagu.dw@gmail.com>
2020-09-22 20:20:41 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact_us')
+def contact():
+    return render_template('contact.html')
